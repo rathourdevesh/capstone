31acc04c6cdfaf0a665fd7316626a728306a4a89
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2023-10-07 00:28:00 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@ sudo apt-get update -y
 sudo apt-get install git -y
 git clone https://github.com/rathourdevesh/capstone.git
 cd capstone
-git checkout typing-text
+git checkout typing-test
 sudo apt-get install python3 -y
 sudo apt-get -y install python3-pip -y
 python3 -m venv venv
 source venv/bin/activate
-pip install requirements.txt
+pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
