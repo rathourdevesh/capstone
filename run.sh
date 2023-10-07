5aefc800915725b0b51096595b2eb5541b4c3f7a
rathourdevesh <36456675+rathourdevesh@users.noreply.github.com>
2023-10-07 10:39:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ source venv/bin/activate
 pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver
+python3 manage.py runserver 0.0.0.0:8000
