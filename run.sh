5e4ce7e922729fa6bfaba98e840bedb1ab5d8301
rathourdevesh <rathourdevesh@gmail.com>
2023-10-08 16:24:42 +0530

    nginx conf

--- run.sh
+++ run.sh
@@ -12,3 +12,4 @@ pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
+gunicorn capstone.wsgi:application --bind 0.0.0.0:8000
